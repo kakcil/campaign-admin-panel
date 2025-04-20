45028ab0bf55263d8559a9b2adb797b670c1429f
Kemal AKCIL <kemalakcil@outlook.com>
2025-04-20 21:59:03 +0300

    feat(campaign-create): adds campaign creation form with localStorage

--- src/app/pages/campaign-create/campaign-create.component.ts
+++ src/app/pages/campaign-create/campaign-create.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-campaign-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './campaign-create.component.html',
   styleUrl: './campaign-create.component.scss'
 })
 export class CampaignCreateComponent {
+  title: string = '';
+  description: string = '';
+  successMessage: string = '';
 
+  createCampaign() {
+    const newCampaign = {
+      id: Date.now(),
+      title: this.title,
+      description: this.description,
+      score: 0,
+      date: new Date().toISOString().split('T')[0]
+    };
+
+    const campaigns = JSON.parse(localStorage.getItem('campaigns') || '[]');
+    campaigns.push(newCampaign);
+    localStorage.setItem('campaigns', JSON.stringify(campaigns));
+
+    this.successMessage = 'Campaign successfully added.';
+
+    setTimeout(() => {
+      this.successMessage = '';
+    }, 2000);
+
+    this.title = '';
+    this.description = '';
+  }
 }
